77e5680183b81790ac905ffcf35072e4e238c591
rudreshruddra <rudrarudra8075@gmail.com>
2024-07-11 13:18:17 +0530

    Add files via upload

---
+++ matrixsum.java
@@ -0,0 +1,24 @@
+public class matmul{
+    public static void main(String[] args)
+    {
+        int[][] mat1={{1,2},{5,3}};
+        int[][] mat2={{2,3},{4,1}};
+        int[][] result={{0,0},{0,0}};
+        for(int i=0;i<mat1.length;i++)
+        {
+            for(int j=0;j<mat2.length;j++)
+            {
+                    result[i][j]=mat1[i][j]+mat2[i][j];
+            }
+        }
+        for(int j=0;j<mat2.length;j++)
+            {
+                for(int k=0;k<mat2.length;k++)
+                {
+                 System.out.print(result[j][k]+" ");
+                }
+                System.out.println();
+            }
+        
+    }
+}
